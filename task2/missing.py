b31095355997134d5b0d9cb7cc06af21dcc43cc6
safa072004 <asansafa04@gmail.com>
2025-07-11 18:57:40 +0530

    Update missing.py

--- task2/missing.py
+++ task2/missing.py
@@ -1,17 +1,4 @@
-import pytest
-from climb import climb_stairs
-
-@pytest.mark.parametrize("n, expected", [
-    (0, 0),
-    (1, 1),
-    (2, 2),
-    (3, 3),
-    (4, 5),
-    (5, 8),
-    (10, 89),
-    (20, 10946),
-    (-1, 0),       
-    (-10, 0),
-])
-def test_climb_stairs(n, expected):
-    assert climb_stairs(n) == expected
+def find_missing_number(nums, n):
+    expected_sum = n * (n + 1) // 2
+    actual_sum = sum(nums)
+    return expected_sum - actual_sum
