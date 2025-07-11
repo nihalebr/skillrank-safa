22a14262f6912c55eda1ed024490c90667c2ac8f
safa072004 <asansafa04@gmail.com>
2025-07-11 18:32:56 +0530

    Update test_missing.py

--- task2/test_missing.py
+++ task2/test_missing.py
@@ -1,7 +1,5 @@
 import pytest
 from missing import find_missing_number
-
-# Normal test cases
 @pytest.mark.parametrize("nums, n, expected", [
     ([1, 2, 4, 5], 5, 3),
     ([2, 3, 1, 5], 5, 4),
@@ -10,8 +8,6 @@ from missing import find_missing_number
 ])
 def test_find_missing_number(nums, n, expected):
     assert find_missing_number(nums, n) == expected
-
-# Edge case: wrong size
 def test_missing_number_invalid_size():
     with pytest.raises(ValueError):
         find_missing_number([1, 2, 3], 5)
